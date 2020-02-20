62386062347fe4e7d8675675e5b8f28996b7e349
Sergii.Shvets <sergii.shvets@thomsonreuters.com>
2020-02-20 20:35:15 +0200

    added article

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Article {
+  id: string;
+  heading: string;
+  date: string;
+  author: string;
+  shortDescription: string;
+  content: string;
+  sourceUrl: string;
+}
+
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
@@ -7,10 +17,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticleComponent implements OnInit {
 
+  articles: Article[] = [
+    {id: '0', heading: 'Reuters', date: '2/20/2020', author: 'Jack London', shortDescription: 'Description', content: 'Some content', sourceUrl: 'http://someurl.com'},
+    {id: '1', heading: 'Reuters', date: '2/20/2020', author: 'Jack London', shortDescription: 'Description', content: 'Some content', sourceUrl: 'http://someurl.com'},
+    {id: '2', heading: 'Reuters', date: '2/20/2020', author: 'Jack London', shortDescription: 'Description', content: 'Some content', sourceUrl: 'http://someurl.com'}
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
-
-
 }
