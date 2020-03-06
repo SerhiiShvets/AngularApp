ec61e2773c9da888568da00f8f7bcde05b360d35
Sergii.Shvets <sergii.shvets@thomsonreuters.com>
2020-03-06 17:24:14 +0200

    fixed create-article

--- src/app/news-feed/news-feed.module.ts
+++ src/app/news-feed/news-feed.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NewsFeedComponent } from './news-feed.component';
 import { FilterModule } from '../filter/filter.module';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 @NgModule({
-    imports:      [ BrowserModule, FilterModule],
+    imports:      [ BrowserModule, FilterModule, Ng2SearchPipeModule],
     declarations: [ NewsFeedComponent],
     bootstrap:    [ NewsFeedComponent ],
     exports:      [ NewsFeedComponent ]
