47ad8a99dcdbb4b83ec0c1bf30a53315952b6144
Serhii Shvets <sergius.shvets@gmail.com>
2020-03-22 13:12:19 +0200

    just commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {Routes, RouterModule} from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +19,6 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import { MatNativeDateModule } from '@angular/material/core';
 import { ArticlesDataService } from './shared/articles-data.service';
-import { from } from 'rxjs';
 import { SourcesSelectorComponent } from './sources-selector/sources-selector.component';
 import { FilterPipe } from './filter.pipe';
 
